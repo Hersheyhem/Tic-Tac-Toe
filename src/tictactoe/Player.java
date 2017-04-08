33bfddd01b449b20dec0176e0b451392685d99f3
Hersheyhem <hemlatamahaur96@gmail.com>
2017-04-08 23:08:22 +0530

    Update Player.java

--- src/tictactoe/Player.java
+++ src/tictactoe/Player.java
@@ -5,10 +5,8 @@ import java.util.Scanner;
 public class Player {
 
 	private String playerName;
-	private Places places;
-	private int row;
-	private int column;
-	private Board board;
+
+	private static Board board;
 
 	public Player(Board board) {
 		this.board = board;
@@ -22,14 +20,6 @@ public class Player {
 		this.playerName = playerName;
 	}
 
-	public Places getPlace() {
-		return places;
-	}
-
-	public void setPlace(Places places) {
-		this.places = places;
-	}
-
 	public void setX(int row, int column) {
 		board.grid[row][column] = "X";
 	}
@@ -39,18 +29,26 @@ public class Player {
 	}
 
 	public static void startGame(Player player, Player player2) {
-		Scanner input = new Scanner(System.in);
-		System.out.println("choose X or Y");
-		player.setPlace(Places.X);
-		int row = input.nextInt();
-		int column = input.nextInt();
-		player.setX(row, column);
-                System.out.println("choose X or Y");
-		player2.setPlace(Places.Y);
-		int row2 = input.nextInt();
-		int column2 = input.nextInt();
-		player2.setY(row2, column2);
+
+		while (!board.getHorizontally() && !board.getDiagonally() && !board.getVertically()) {
+			Scanner input = new Scanner(System.in);
+			System.out.println("choose X or Y");
+
+			int row = input.nextInt();
+			int column = input.nextInt();
+			player.setX(row, column);
+			board.checkIfTrue();
+			System.out.println("choose X or Y");
+
+			int row2 = input.nextInt();
+			int column2 = input.nextInt();
+			player2.setY(row2, column2);
+			board.checkIfTrue();
+			System.out.println("D" + board.getDiagonally());
+			System.out.println("V" + board.getVertically());
+			System.out.println("H" + board.getHorizontally());
+			board.getArray();
+		}
 	}
+
 }
-
-
