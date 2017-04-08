1e06e9577e7cb3a204e49b137d7802b31dfc886c
Hersheyhem <hemlatamahaur96@gmail.com>
2017-04-08 23:20:28 +0530

    Update Player.java

--- src/tictactoe/Player.java
+++ src/tictactoe/Player.java
@@ -4,20 +4,10 @@ import java.util.Scanner;
 
 public class Player {
 
-	private String playerName;
-
 	private static Board board;
 
 	public Player(Board board) {
 		this.board = board;
-	}
-
-	public String getName() {
-		return playerName;
-	}
-
-	public void setName(String playerName) {
-		this.playerName = playerName;
 	}
 
 	public void setX(int row, int column) {
@@ -32,21 +22,21 @@ public class Player {
 
 		while (!board.getHorizontally() && !board.getDiagonally() && !board.getVertically()) {
 			Scanner input = new Scanner(System.in);
-			
+			System.out.println("Welcome to tic tac toe.");
 
 			int row = input.nextInt();
 			int column = input.nextInt();
 			player.setX(row, column);
 			board.checkIfTrue();
-			
 
 			int row2 = input.nextInt();
 			int column2 = input.nextInt();
 			player2.setY(row2, column2);
 			board.checkIfTrue();
-		
+
 			board.getArray();
 		}
 	}
 
 }
+
