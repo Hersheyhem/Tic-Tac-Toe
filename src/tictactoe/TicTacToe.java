f15a29821b1857a85f5c0f63a2493bebab75279c
Hersheyhem <hemlatamahaur96@gmail.com>
2017-04-08 19:24:58 +0530

    Update TicTacToe.java

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -3,6 +3,11 @@ package tictactoe;
 public class TicTacToe {
 
 	public static void main(String[] args) {
-		
+
+		Board board = new Board();
+		Player player = new Player(board);
+		Player player2 = new Player(board);
+		Player.startGame(player, player2);
+
 	}
 }
